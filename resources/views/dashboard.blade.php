890a1da6822b9fc89293d4d853ea294d148082f0
rindrajosia <rrindrajosia@gmail.com>
2022-08-20 19:19:46 +0300

    Read data from eloquent ORM

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Hi... <b>{{ Auth::user()->name }}</b>
+            <b style="float:right;"> Total Users: <span class="badge badge-danger">{{ count($users) }}</span></b>
         </h2>
     </x-slot>
 
@@ -18,12 +19,15 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                  </tr>
+                  @php($i = 1)
+                  @foreach($users as $user)
+                    <tr>
+                      <th scope="row">{{$i++}}</th>
+                      <td>{{$user->name}}</td>
+                      <td>{{$user->email}}</td>
+                      <td>{{$user->created_at->diffForHumans()}}</td>
+                    </tr>
+                  @endforeach
                 </tbody>
               </table>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,6 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    $users = User::all();
+    return view('dashboard', compact('users'));
 })->name('dashboard');
